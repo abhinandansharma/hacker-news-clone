c0ef11d332ef1bfd42aa7503ce8bc9e7d709148f
Abhinandan Sharma <abhi@novyte.in>
2026-09-08 22:59:42 +0530

    Paper-style HN icon replaces the default favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,7 +17,6 @@ export const metadata: Metadata = {
   description,
   openGraph: { type: 'website', url: 'https://abhinandansharma.github.io/hacker-news-clone/', title: 'Hacker News Reader', description, images: [{ url: 'og.png', width: 1200, height: 630 }] },
   twitter: { card: 'summary_large_image', creator: '@notjustadev', title: 'Hacker News Reader', description, images: ['og.png'] },
-  icons: { icon: 'favicon.ico' },
 };
 
 export const viewport: Viewport = { themeColor: '#f4f1e8' };
